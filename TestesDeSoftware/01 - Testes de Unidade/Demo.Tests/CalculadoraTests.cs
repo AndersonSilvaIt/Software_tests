ece8aee22d45df564eb0e3e097518323d39160d4
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2023-02-20 17:37:14 -0300

    Criado o projeto inicial do projeto de testes

---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultador = calculadora.Somar(2, 2);
+
+            // Primeiro parâmetro, o resultado esperado
+            // o segundo, o valor obtido
+            //Assert
+            Assert.Equal(4, resultador);
+        }
+    }
+}
