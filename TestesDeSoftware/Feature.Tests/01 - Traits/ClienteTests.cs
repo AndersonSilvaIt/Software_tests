b4638066e6a19deb51fc864d43674eb5cf17cfd2
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2023-02-21 11:44:46 -0300

    Usando os traits

---
+++ TestesDeSoftware/Feature.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,52 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests._01___Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Client Trati Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                                        id: Guid.NewGuid(),
+                                        nome: "Anderson",
+                                        sobrenome: "Mota",
+                                        dataNascimento: DateTime.Now.AddYears(-28),
+                                        email: "anderson@gmail.com",
+                                        ativo: true,
+                                        dataCadastro: DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Client Trati Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                                        id: Guid.NewGuid(),
+                                        nome: "",
+                                        sobrenome: "",
+                                        dataNascimento: DateTime.Now,
+                                        email: "anderson2gmail.com",
+                                        ativo: true,
+                                        dataCadastro: DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
