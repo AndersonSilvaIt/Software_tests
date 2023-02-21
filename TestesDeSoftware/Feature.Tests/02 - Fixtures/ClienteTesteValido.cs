3ab251f3263b09a98c1f09b7d94c5a21bf479509
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2023-02-21 13:06:50 -0300

    Ordenação de testes

---
+++ TestesDeSoftware/Feature.Tests/02 - Fixtures/ClienteTesteValido.cs
@@ -0,0 +1,31 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests._02___Fixtures
+{
+    [Collection(nameof(ClienteCollection))]
+    public class ClienteTesteValido
+    {
+        private readonly ClienteTestsFixture _clienteTestsFixture;
+
+        public ClienteTesteValido(ClienteTestsFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Client Trati Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
---
+++ TestesDeSoftware/Feature.Tests/02 - Fixtures/CllienteTestsFixture.cs
@@ -0,0 +1,43 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests._02___Fixtures
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>  { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                                        id: Guid.NewGuid(),
+                                        nome: "Anderson",
+                                        sobrenome: "Mota",
+                                        dataNascimento: DateTime.Now.AddYears(-28),
+                                        email: "anderson@gmail.com",
+                                        ativo: true,
+                                        dataCadastro: DateTime.Now);
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var cliente = new Cliente(
+                            id: Guid.NewGuid(),
+                            nome: "",
+                            sobrenome: "",
+                            dataNascimento: DateTime.Now,
+                            email: "anderson2gmail.com",
+                            ativo: true,
+                            dataCadastro: DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
