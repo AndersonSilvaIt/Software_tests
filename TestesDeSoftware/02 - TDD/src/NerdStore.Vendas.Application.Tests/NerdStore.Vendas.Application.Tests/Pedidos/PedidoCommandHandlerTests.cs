53264249a2dbffa76fdad9d7b6bf278802256315
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2023-06-08 08:35:54 -0300

    Implementando os Handlers - Parte 1

---
+++ TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Application.Tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Vendas.Application.Commands;
+using Xunit;
+using Moq.AutoMock;
+using Moq;
+using NerdStore.Vendas.Domain;
+using MediatR;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class PedidoCommandHandlerTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido com Sucesso")]
+        [Trait("Categoria", "Vendas - Pedido Command Handler")]
+        public void AdicionarItem_NovoPedido_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            var mocker = new AutoMocker();
+
+            //Resolve todas as injeções de dependência automaticamente
+            var pedidoHandler = mocker.CreateInstance<PedidoCommandHandler>();
+
+            // Act
+            var result = pedidoHandler.Handle(pedidoCommand);
+
+            // Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(r => r.Adicionar(It.IsAny<Pedido>()), Times.Once);
+            mocker.GetMock<IMediator>().Verify(r => r.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+    }
+}
